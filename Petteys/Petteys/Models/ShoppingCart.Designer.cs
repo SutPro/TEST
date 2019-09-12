f7a8f1b463ffee93b35a3ed1f6dadcfb8202183b
Phoommin Naksenee <B6070489@office365.sut.ac.th>
2019-09-12 22:54:32 +0700

    sdsdsdssdsdsd

--- Petteys/Petteys/Models/ShoppingCart.Designer.cs
+++ Petteys/Petteys/Models/ShoppingCart.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\EAK_FORORIDA\documents\visual studio 2015\Projects\Petteys\Petteys\Models\ShoppingCart.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\B6070\Desktop\Petey\TEST\Petteys\Petteys\Models\ShoppingCart.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
